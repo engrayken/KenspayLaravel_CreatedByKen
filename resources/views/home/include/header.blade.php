88b3922ad17ccd77a92245daed45624af073d379
engrayken <48656985+kenneth2718@users.noreply.github.com>
2024-04-19 09:11:41 +0100

    firday morning

--- resources/views/home/include/header.blade.php
+++ resources/views/home/include/header.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <base href="http://127.0.0.1:8000">
+    <base href="{{ env('APP_URL') }}">
     </base>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
